5aeb63eccdbe02bde1352694db8c384a6424508a
Slaven Rezic <slaven@rezic.de>
2005-05-12 20:50:14 +0000

    start problems?

--- WWWBrowser.pm
+++ WWWBrowser.pm
@@ -2,7 +2,7 @@
 # -*- perl -*-
 
 #
-# $Id: WWWBrowser.pm,v 2.26 2005/04/16 01:30:45 eserte Exp $
+# $Id: WWWBrowser.pm,v 2.27 2005/05/12 20:50:14 eserte Exp $
 # Author: Slaven Rezic
 #
 # Copyright (C) 1999,2000,2001,2003,2005 Slaven Rezic. All rights reserved.
@@ -19,7 +19,7 @@ use strict;
 use vars qw(@unix_browsers $VERSION $VERBOSE $initialized $os $fork
 	    $got_from_config $ignore_config);
 
-$VERSION = sprintf("%d.%02d", q$Revision: 2.26 $ =~ /(\d+)\.(\d+)/);
+$VERSION = sprintf("%d.%02d", q$Revision: 2.27 $ =~ /(\d+)\.(\d+)/);
 
 @unix_browsers = qw(_internal_htmlview
 		    _default_gnome _default_kde
@@ -57,7 +57,9 @@ sub start_browser {
     if ($os eq 'win') {
 	if (!eval 'require Win32Util;
 	           Win32Util::start_html_viewer($url)') {
-	    # if this fails, just try the url only
+	    # If this fails, just try the url only.
+	    # XXX There are reports that "start" and Tk programms
+	    # do not work well together (slow startup and such).
 	    system("start $url");
 	    if ($?/256 != 0) {
 		# otherwise explicitely use explorer
